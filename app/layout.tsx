ccf049e92e3399c67e5b92ddfb626202ae793507
DD <ddhar1219@gmail.com>
2025-04-11 22:13:59 +0600

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import './globals.css'
+import { GeistMono } from 'geist/font/mono'
+
+export const metadata = {
+  title: 'three in a row',
+  description: 'a simple tic-tac-toe game built with next.js',
+    generator: 'v0.dev'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en" className={GeistMono.className}>
+      <body className="bg-black">{children}</body>
+    </html>
+  )
+}
+
+
+import './globals.css'
