767c7b31d38995d0482ca4ba07d628da538a21db
DD prog <dipanjandhar198@gmail.com>
2025-04-12 09:18:29 +0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Game from './components/Game'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-black">
-      <h1 className="text-white">Created By Dipanjan Dhar</h1>
+      <h1 className="text-white">Create By Dipanjan Dhar</h1>
       <Game />
     </main>
   )
